9ab76a5c7e04f2415283725090b37ca59a9baa4a
FyreHost <23saahilt@students.harker.org>
2021-08-03 19:34:57 -0700

    Update AdvancedFlags.java

--- src/main/java/me/mrxbox98/advancedflags/AdvancedFlags.java
+++ src/main/java/me/mrxbox98/advancedflags/AdvancedFlags.java
@@ -4,16 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdvancedFlags extends JavaPlugin implements LogHelper {
 
-    public static JavaPlugin instance;
-
     @Override
     public void onEnable() {
         System.out.println(getServer().getVersion());
 
         if(getServer().getVersion().contains("1.8.8"))
         {
-            instance=new me.mrxbox98.v1_8_8.advancedflags.AdvancedFlags();
-            instance.onEnable();
+            getServer().getPluginManager().disablePlugin(this);
+            getServer().getPluginManager().enablePlugin(new me.mrxbox98.v1_8_8.advancedflags.AdvancedFlags());
         }
     }
 
