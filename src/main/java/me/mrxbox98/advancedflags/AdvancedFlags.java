068983d6236a2a1f818bb9bb32975bae3b439f87
FyreHost <23saahilt@students.harker.org>
2021-08-04 13:50:01 -0700

    Update AdvancedFlags.java

--- src/main/java/me/mrxbox98/advancedflags/AdvancedFlags.java
+++ src/main/java/me/mrxbox98/advancedflags/AdvancedFlags.java
@@ -10,6 +10,7 @@ import me.mrxbox98.advancedflags.flags.Flag;
 import me.mrxbox98.advancedflags.listeners.MainListener;
 import me.mrxbox98.advancedflags.utils.AdvancedPlayer;
 import me.mrxbox98.advancedflags.flags.FlagManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,6 +18,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public final class AdvancedFlags extends JavaPlugin implements LogHelper {
@@ -93,41 +95,46 @@ public final class AdvancedFlags extends JavaPlugin implements LogHelper {
 
         HashMap<String, String> hashMap = new HashMap<>();
 
-        Gson gson = new Gson();
-
         System.out.println(GREEN_BACKGROUND+"↱   STARTED FLAG LINKING   ↰");
 
         for(Flag flag: FlagManager.flags)
         {
-            try
-            {
-                String str = readJsonFromUrl("http://api.worldbank.org/v2/country/" +flag.abbr.toLowerCase()+ "?format=json");
 
-                int start = str.indexOf("name")+7;
+            Bukkit.getScheduler().runTaskAsynchronously(instance, new Runnable() {
+                @Override
+                public void run() {
+                    try
+                    {
+                        String str = readJsonFromUrl("http://api.worldbank.org/v2/country/" +flag.abbr.toLowerCase()+ "?format=json");
 
-                int end = str.substring(start).indexOf(",")-1+start;
+                        int start = str.indexOf("name")+7;
 
-                String name = str.substring(start,end);
+                        int end = str.substring(start).indexOf(",")-1+start;
 
-                hashMap.put(flag.abbr, name);
+                        String name = str.substring(start,end);
 
-                System.out.println(GREEN_BACKGROUND+"|           "+flag.abbr+"           |");
-            }
-            catch(Exception | Error e)
-            {
-                e.printStackTrace();
-            }
+                        hashMap.put(flag.abbr, name);
 
+                        System.out.println(GREEN_BACKGROUND+"|           "+flag.abbr+"           |");
+                    }
+                    catch(Exception | Error e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
         }
 
         return hashMap;
     }
 
+
+
     public static String readJsonFromUrl(String url) throws Exception {
         InputStream is = new URL(url).openStream();
         try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             return readAll(rd);
         } finally {
             is.close();
