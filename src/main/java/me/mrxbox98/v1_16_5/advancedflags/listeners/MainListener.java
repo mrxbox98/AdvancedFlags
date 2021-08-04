b52f1913ecb454eca3dab7719629befd6762dbc2
FyreHost <23saahilt@students.harker.org>
2021-08-03 18:52:58 -0700

    Added rest of versions

---
+++ src/main/java/me/mrxbox98/v1_16_5/advancedflags/listeners/MainListener.java
@@ -0,0 +1,30 @@
+package me.mrxbox98.v1_16_5.advancedflags.listeners;
+
+import me.mrxbox98.v1_16_5.advancedflags.utils.AdvancedPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class MainListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event)
+    {
+        AdvancedPlayer.advancedPlayers.add(new AdvancedPlayer(event.getPlayer()));
+    }
+
+    @EventHandler
+    public void onPlayerKickEvent(PlayerKickEvent event)
+    {
+        AdvancedPlayer.advancedPlayers.remove(AdvancedPlayer.getAdvancedPlayer(event.getPlayer()));
+    }
+
+    @EventHandler
+    public void onPlayerQuitEvent(PlayerQuitEvent event)
+    {
+        AdvancedPlayer.advancedPlayers.remove(AdvancedPlayer.getAdvancedPlayer(event.getPlayer()));
+    }
+
+}
