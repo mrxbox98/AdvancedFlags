f6674ccb0ec2cebe15315489710c02accc12418e
FyreHost <23saahilt@students.harker.org>
2021-08-05 15:21:01 -0700

    Update AdvancedFlagGui.java

--- src/main/java/me/mrxbox98/advancedflags/flags/AdvancedFlagGui.java
+++ src/main/java/me/mrxbox98/advancedflags/flags/AdvancedFlagGui.java
@@ -202,8 +202,6 @@ public class AdvancedFlagGui implements Listener, LogHelper {
         // Using slots click is a best option for your inventory click's
         AdvancedPlayer.getAdvancedPlayer(p).flagId=e.getRawSlot()+(page-1)*45;
 
-        System.out.println(page);
-
         send(p,"Equiped flag id: " +(e.getRawSlot()+(page-1)*45));
     }
 
