c6a5d20ebe732a444bbff861e3464c452363cf5e
FyreHost <23saahilt@students.harker.org>
2021-08-04 20:53:20 -0700

    Fixed config

--- src/main/java/me/mrxbox98/advancedflags/config/AdvancedConfig.java
+++ src/main/java/me/mrxbox98/advancedflags/config/AdvancedConfig.java
@@ -8,11 +8,18 @@ public class AdvancedConfig {
 
     public static boolean glowingFlag = false;
 
+    public static double heightFromPlayer = 4d;
+
     public static void setupConfig()
     {
         AdvancedFlags.getInstance().getConfig().addDefault("Delay",2);
         AdvancedFlags.getInstance().getConfig().addDefault("GlowingFlag",false);
+        AdvancedFlags.getInstance().getConfig().addDefault("HeightFromPlayer",4d);
         AdvancedFlags.getInstance().saveDefaultConfig();
+
+        delay=AdvancedFlags.getInstance().getConfig().getInt("Delay");
+        glowingFlag=AdvancedFlags.getInstance().getConfig().getBoolean("GlowingFlag");
+        heightFromPlayer=AdvancedFlags.getInstance().getConfig().getDouble("HeightFromPlayer");
     }
 
 }
